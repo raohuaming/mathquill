766061b9b23337611bec6f529947f873660f8703
Huaming Rao <huaming.rao@gmail.com>
2015-11-19 17:02:30 +0800

    change the relative path in package.js

--- meteor/package.js
+++ meteor/package.js
@@ -1,7 +1,7 @@
 Package.describe({
   summary: "What this does",
   version: "0.9.5",
-  name: "mathquill:mathquill",
+  name: "huaming:mathquill",
   git: "https://github.com/mathquill/mathquill.git"
 });
 
@@ -12,38 +12,38 @@ Package.onUse(function (api) {
   api.use( 'less' );
 
   api.addFiles( [
-    '../build/font/stixgeneral-bundle/STIXFontLicense2010.txt',
-    '../build/font/stixgeneral-bundle/stixgeneral-webfont.eot',
-    '../build/font/stixgeneral-bundle/stixgeneral-webfont.svg',
-    '../build/font/stixgeneral-bundle/stixgeneral-webfont.ttf',
-    '../build/font/stixgeneral-bundle/stixgeneral-webfont.woff',
-    '../build/font/stixgeneral-bundle/stixgeneralbol-webfont.eot',
-    '../build/font/stixgeneral-bundle/stixgeneralbol-webfont.svg',
-    '../build/font/stixgeneral-bundle/stixgeneralbol-webfont.ttf',
-    '../build/font/stixgeneral-bundle/stixgeneralbol-webfont.woff',
-    '../build/font/stixgeneral-bundle/stixgeneralbolita-webfont.eot',
-    '../build/font/stixgeneral-bundle/stixgeneralbolita-webfont.svg',
-    '../build/font/stixgeneral-bundle/stixgeneralbolita-webfont.ttf',
-    '../build/font/stixgeneral-bundle/stixgeneralbolita-webfont.woff',
-    '../build/font/stixgeneral-bundle/stixgeneralitalic-webfont.eot',
-    '../build/font/stixgeneral-bundle/stixgeneralitalic-webfont.svg',
-    '../build/font/stixgeneral-bundle/stixgeneralitalic-webfont.ttf',
-    '../build/font/stixgeneral-bundle/stixgeneralitalic-webfont.woff',
-    '../build/font/Symbola.eot',
-    '../build/font/Symbola.otf',
-    '../build/font/Symbola.svg',
-    '../build/font/Symbola.ttf',
-    '../build/font/Symbola.woff'
+    'build/font/stixgeneral-bundle/STIXFontLicense2010.txt',
+    'build/font/stixgeneral-bundle/stixgeneral-webfont.eot',
+    'build/font/stixgeneral-bundle/stixgeneral-webfont.svg',
+    'build/font/stixgeneral-bundle/stixgeneral-webfont.ttf',
+    'build/font/stixgeneral-bundle/stixgeneral-webfont.woff',
+    'build/font/stixgeneral-bundle/stixgeneralbol-webfont.eot',
+    'build/font/stixgeneral-bundle/stixgeneralbol-webfont.svg',
+    'build/font/stixgeneral-bundle/stixgeneralbol-webfont.ttf',
+    'build/font/stixgeneral-bundle/stixgeneralbol-webfont.woff',
+    'build/font/stixgeneral-bundle/stixgeneralbolita-webfont.eot',
+    'build/font/stixgeneral-bundle/stixgeneralbolita-webfont.svg',
+    'build/font/stixgeneral-bundle/stixgeneralbolita-webfont.ttf',
+    'build/font/stixgeneral-bundle/stixgeneralbolita-webfont.woff',
+    'build/font/stixgeneral-bundle/stixgeneralitalic-webfont.eot',
+    'build/font/stixgeneral-bundle/stixgeneralitalic-webfont.svg',
+    'build/font/stixgeneral-bundle/stixgeneralitalic-webfont.ttf',
+    'build/font/stixgeneral-bundle/stixgeneralitalic-webfont.woff',
+    'build/font/Symbola.eot',
+    'build/font/Symbola.otf',
+    'build/font/Symbola.svg',
+    'build/font/Symbola.ttf',
+    'build/font/Symbola.woff'
   ], 'client', { isAsset: true } );
 
   api.addFiles( [
-    '../build/mathquill.css',
-    '../build/mathquill.min.js'
+    'build/mathquill.css',
+    'build/mathquill.min.js'
   ], 'client' );
 });
 
 Package.onTest(function (api) {
-  api.use('mathquill:mathquill');
-  api.use('tinytest@1.0.0');
-  api.addFiles('test.js', 'client');
+  api.use('huaming:mathquill');
+  api.use('tinytest');
+  api.addFiles('meteor/test.js', 'client');
 });
